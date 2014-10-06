caa00f4b8b9d3433cefab6675294de3279b38e6b
Hackbright Student <info@hackbrightacademy.com>
2014-10-06 15:08:18 -0700

    Completed first part of Extra Credit.

--- wordcount.py
+++ wordcount.py
@@ -1,4 +1,5 @@
 from sys import argv
+import string
 
 word_counts = {}
 
@@ -8,6 +9,7 @@ with open(filename) as f:
     for line in f:
         words_in_line = line.strip().split()
         for word in words_in_line:
+            word = word.strip(string.punctuation)
             word_counts[word] = word_counts.get(word, 0) + 1
 for word, count in word_counts.items():
     print word, count
