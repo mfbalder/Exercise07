6e06563645b5e59cf24386d34f3b234bea8e207d
Hackbright Student <info@hackbrightacademy.com>
2014-10-06 15:21:23 -0700

    Completed part three of the Extra Credit

--- wordcount.py
+++ wordcount.py
@@ -11,5 +11,11 @@ with open(filename) as f:
         for word in words_in_line:
             word = word.strip(string.punctuation).lower()
             word_counts[word] = word_counts.get(word, 0) + 1
-for word, count in word_counts.items():
-    print word, count
+
+
+for word in sorted(word_counts, key=word_counts.get, reverse=True):
+    print word, word_counts[word]
+
+#for word in sorted(word_counts, cmp=lambda x,y: cmp(word_counts[x], word_counts[y]), reverse=True):
+#    print word, word_counts[word]
+
