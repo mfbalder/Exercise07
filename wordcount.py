31bd43e9c885a2ed1b9944e02be0007f6e89ebf1
Hackbright Student <info@hackbrightacademy.com>
2014-10-06 16:35:57 -0700

    Completed the Extra Credit in a less obtuse way.

--- wordcount.py
+++ wordcount.py
@@ -13,9 +13,18 @@ with open(filename) as f:
             word_counts[word] = word_counts.get(word, 0) + 1
 
 
-for word in sorted(word_counts, key=word_counts.get, reverse=True):
+for word in sorted(sorted(word_counts), key=word_counts.get, reverse=True):
     print word, word_counts[word]
 
 #for word in sorted(word_counts, cmp=lambda x,y: cmp(word_counts[x], word_counts[y]), reverse=True):
 #    print word, word_counts[word]
 
+# frequency_dict = {}
+# for word in word_counts:
+#     frequency = word_counts.get(word)
+#     frequency_dict[frequency] = frequency_dict.get(frequency, []) + [word]
+
+# for frequency in sorted(frequency_dict, reverse=True):
+#     print_list = frequency_dict.get(frequency)
+#     for word in sorted(print_list):
+#        print word, frequency
