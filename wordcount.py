f79f11483ca6008eba96398e045c13481f56f746
Hackbright Student <info@hackbrightacademy.com>
2014-10-06 15:09:09 -0700

    Completed part two of Extra Credit

--- wordcount.py
+++ wordcount.py
@@ -9,7 +9,7 @@ with open(filename) as f:
     for line in f:
         words_in_line = line.strip().split()
         for word in words_in_line:
-            word = word.strip(string.punctuation)
+            word = word.strip(string.punctuation).lower()
             word_counts[word] = word_counts.get(word, 0) + 1
 for word, count in word_counts.items():
     print word, count
